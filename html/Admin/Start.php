c87f6386f02e380f768f17dc0d22df9e163a47be
Treiniota <karolis.breidokas@ktu.edu>
2017-02-05 20:02:36 +0200

    updated formating. changed automatic question reset

---
+++ html/Admin/Start.php
@@ -0,0 +1,17 @@
+<?php
+    ini_set('display_errors', 'On');
+    session_start();
+    header('Content-Type: text/event-stream');
+    header('Cache-Control: no-cache');
+    $mysqli = new mysqli($_SESSION['dbhost'],$_SESSION['dbuser'],$_SESSION['dbpass'],$_SESSION['db']);
+    function sendMsg() {
+      echo "id: 100 \n" . PHP_EOL;
+      echo "data: 100 \n\n" . PHP_EOL;
+      echo PHP_EOL;
+    }
+    $ret=$mysqli->query("SELECT reset FROM ServerInfo");
+    $row=$ret->fetch_row();
+    if(!$row['0']){
+        sendMsg();
+    }
+?>
